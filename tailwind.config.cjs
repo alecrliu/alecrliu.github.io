a55ab2952bbe37f8f039b17fc843380dda1700d8
Alec Liu <alec.liu@utexas.edu>
2024-10-28 21:20:35 +0000

    add tailwind and skeletonui

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,svelte,ts,css}',
+    './node_modules/@skeletonlabs/skeleton/**/*.svelte'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

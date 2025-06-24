567b77eaa99e944023192d81c487df8ab738c755
aliu481 <alec.liu@gatech.edu>
2025-06-23 21:17:03 -0500

    not sure why the page is blank

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # alecrliu.github.io
 My website for portfolios, resumes, and other experiments.
 
+https://alecrliu.github.io
+
 Made with Sveltekit, TailwindCSS, and SkeletonUI.
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,11 @@
     import "../app.css";
     import { AppBar } from "$lib";
 </script>
-
-<AppBar />
-<slot></slot>
-
 <style>
     :global(body) {
-        background: linear-gradient(135deg, #f8fafc 0%, #e0e7ef 100%);
+        background-color: #f0f4f8;
+        color: #333;
     }
 </style>
+<AppBar />
+<slot></slot>
